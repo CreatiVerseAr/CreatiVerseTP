9d3907e0c245fd067b8b3f70d4638e6deb681ce2
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-05-25 03:41:06 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,29 +1,23 @@
 function validarFormulario() {
     // Obtener los valores de los campos de entrada
     var nombre = $("#nombre").val();
-    var email = $("#email").val();
+    var apellido = $("#apellido").val();
     
-    // validar el formato de correo electrónico
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+    
     // Validar los datos
-    if (nombre.trim() === "" || email.trim() === "") {
+    if (nombre.trim() === "" || apellido.trim() === "") {
         alert("Por favor, complete todos los campos.");
         return false;
     }
-    // Validar correo electrónico
-    if (!emailRegex.test(email)) {
-        alert("Por favor, introduzca un correo electrónico válido.");
-        return false;
-    }
-
-    alert("¡Formulario válido! Puedes realizar alguna acción aquí, como mostrar un mensaje de éxito.");
+    
+    
+    alert("¡Formulario válido!");
 
     // Evitar que el formulario se envíe automáticamente
     return false;
 }
 
-// Agregar un listener para el evento submit del formulario
+// Agregar  listener para el evento submit del formulario
 $(document).ready(function() {
     $("#registroForm").submit(function(event) {
         // Detener el envío del formulario si la validación no pasa
