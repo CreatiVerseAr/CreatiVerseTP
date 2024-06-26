b3afe1c7b7d4fd9b05e6da021f705c156444fe0a
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-25 21:43:23 -0300

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password', // Cambia esto si es necesario
+  database: 'RegistroClientes'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL server.');
+});
+
+module.exports = db;
