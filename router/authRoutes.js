b2b93ee5b1a5f4532dabfd5393230898cad67121
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:13:42 -0300

    Delete authRoutes.js

--- router/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.register);
-
-// Se pueden agregar más rutas como login, etc.
-
-module.exports = router;
