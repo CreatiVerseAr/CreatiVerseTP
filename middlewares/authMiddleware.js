5e1281cba84e60ad44c0e7f6330915531e4043c4
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:15:38 -0300

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   try {
-    const decoded = jwt.verify(token, 'your_jwt_secret_key'); // Cambia 'your_jwt_secret_key' por tu clave secreta
+    const decoded = jwt.verify(token, 'your_jwt_secret_key'); 
     req.user = decoded;
     next();
   } catch (ex) {
