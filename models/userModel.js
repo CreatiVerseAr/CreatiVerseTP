83120e905d1715154c4c9e97534a2c44f3a46303
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:13:30 -0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,21 @@
-const db = require('../config/config');
+const { db } = require('../config/config');
+
 
 const User = {
-  create: (data, callback) => {
-    const query = `INSERT INTO Clientes (nombre, apellido, genero, email, password, nivel) VALUES (?, ?, ?, ?, ?, ?)`;
-    db.query(
-      query,
-      [data.nombre, data.apellido, data.genero, data.email, data.password, data.nivel],
-      callback
-    );
+  create: (newUser, callback) => {
+    const query = 'INSERT INTO users (nombre, apellido, genero, email, password, nivel) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(query, [newUser.nombre, newUser.apellido, newUser.genero, newUser.email, newUser.password, newUser.nivel], callback);
   },
-  // Otros métodos como find, update, delete pueden ser agregados aca
+  
+  findByEmail: (email, callback) => {
+    const query = 'SELECT * FROM users WHERE email = ?';
+    db.query(query, [email], (err, results) => {
+      if (err) return callback(err, null);
+      if (results.length === 0) return callback(null, null);
+      return callback(null, results[0]);
+    });
+  }
 };
 
 module.exports = User;
+
