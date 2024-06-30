ee8dfff6ef2cb23c9bc17682b204e62e2f8e2223
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:13:14 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,16 +3,16 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password', // Cambiar  si es necesario
-  database: 'RegistroClientes'
+  password: 'Athos88', // Cambia esto si es necesario
+  database: 'registroclientes'
 });
 
 db.connect(err => {
   if (err) {
     console.error('Error connecting to the database:', err);
-    return;
+    process.exit(1);
   }
   console.log('Connected to the MySQL server.');
 });
 
-module.exports = db;
+module.exports = { db };
