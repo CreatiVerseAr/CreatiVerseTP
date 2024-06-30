3d42b3a2e3564da185a930621ef23fa37ac5bbc9
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:13:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./config/config');
-const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
+const mysql = require('mysql2');
+const authRoutes = require('./routes/authRoutes'); 
+const { db } = require ('./config/config');
 
+ 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
-// rutas de autenticación
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.use('/api/auth', authRoutes);
 
-// Archivos estáticos desde la raíz de "public"
-app.use(express.static(__dirname + '/public'));
-
-// Servir las vistas HTML desde la raíz del proyecto
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-app.get('/nosotros', (req, res) => {
-  res.sendFile(__dirname + '/nosotros.html');
-});
-app.get('/cursos', (req, res) => {
-  res.sendFile(__dirname + '/cursos.html');
-});
-app.get('/registro', (req, res) => {
-  res.sendFile(__dirname + '/registro.html');
-});
-
-// Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
