c4b09cf0ec61e05d600f268b130081e48e21bfc5
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-06-30 19:15:31 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Athos88', // Cambia esto si es necesario
+  password: 'Athos88', 
   database: 'registroclientes'
 });
 
