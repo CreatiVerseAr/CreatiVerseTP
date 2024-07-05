780ffd4409cbfe10c8f0d7571c4437a2c768f0b8
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-07-04 21:52:47 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,97 +1,84 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
-const jwtSecret = 'your_jwt_secret'; // Clave secreta directamente en el archivo
+const jwtSecret = 'your_jwt_secret'; 
 
 const register = async (req, res) => {
   const { nombre, apellido, genero, email, password, nivel } = req.body;
-  console.log('Received registration request for:', email);
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = {
-      nombre,
-      apellido,
-      genero,
-      email,
-      password: hashedPassword,
-      nivel
-    };
+    const newUser = { nombre, apellido, genero, email, password: hashedPassword, nivel };
 
     User.create(newUser, (err, result) => {
-      if (err) {
-        console.error('Error registering user:', err);
-        return res.status(500).send('Error al registrar el cliente');
-      }
-      console.log('User registered successfully:', email);
+      if (err) return res.status(500).send('Error al registrar el cliente');
       res.status(201).send('Cliente registrado');
     });
   } catch (error) {
-    console.error('Error al registrar el cliente:', error);
     res.status(500).send('Error al registrar el cliente');
   }
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log('Received login request for:', email);
   try {
     User.findByEmail(email, async (err, user) => {
-      if (err) {
-        console.error('Error finding user:', err);
-        return res.status(500).send('Error al buscar el cliente');
-      }
-      if (!user) {
-        console.log('User not found:', email);
-        return res.status(404).send('Cliente no encontrado');
-      }
+      if (err) return res.status(500).send('Error al buscar el cliente');
+      if (!user) return res.status(404).send('Cliente no encontrado');
 
       const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        console.log('Incorrect password for:', email);
-        return res.status(401).send('Contraseña incorrecta');
-      }
+      if (!isMatch) return res.status(401).send('Contraseña incorrecta');
 
-      const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
-      console.log('User logged in successfully:', email);
+      const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: '1h' });
       res.status(200).json({ token });
     });
   } catch (error) {
-    console.error('Error al intentar iniciar sesión:', error);
     res.status(500).send('Error al intentar iniciar sesión');
   }
 };
 
-//const updateProfile = async (req, res) => {
-  //const { nombre, apellido, genero, nivel } = req.body;
-  //const userId = req.user.id; // ID del usuario autenticado desde el middleware de autenticación
+const updateProfile = async (req, res) => {
+  const { id, nombre, apellido, genero, email, password, nivel } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const updatedUser = { nombre, apellido, genero, email, password: hashedPassword, nivel };
 
-  //try {
-    
-    //const updatedFields = {
-    //  nombre,
-    //  apellido,
-    //  genero,
-    //  nivel
-   // };
+    User.update(id, updatedUser, (err, result) => {
+      if (err) return res.status(500).send('Error al actualizar el cliente');
+      res.status(200).send('Cliente actualizado');
+    });
+  } catch (error) {
+    res.status(500).send('Error al actualizar el cliente');
+  }
+};
+// Para ver los usuarios
+const getUsers = (req, res) => {
+  User.getAll((err, users) => {
+    if (err) return res.status(500).send('Error al obtener los clientes');
+    res.status(200).json(users);
+  });
+};
 
-    // Actualiza el usuario en la base de datos
-    //User.update(userId, updatedFields, (err, result) => {
-    //  if (err) {
-      //  console.error('Error updating user profile:', err);
-      //  return res.status(500).send('Error al actualizar el perfil del usuario');
-      //}
-      //console.log('User profile updated successfully');
-     // res.status(200).send('Perfil actualizado correctamente!');
-   // });
- /// } catch (error) {
-  //  console.error('Error al intentar actualizar el perfil:', error);
-  //  res.status(500).send('Error al intentar actualizar el perfil');
-  ////}
-//};
+const deleteUser = (req, res) => {
+  const userId = req.params.id;
+  console.log('Atencion elimina el usuario del ID:', userId);
+  User.delete(userId, (err, result) => {
+    if (err) {
+      console.error('Error deleting user:', err);
+      return res.status(500).send('Error al eliminar el cliente');
+    }
+    if (result.affectedRows === 0) {
+      console.log('User not found:', userId);
+      return res.status(404).send('Cliente no encontrado');
+    }
+    console.log('User deleted successfully:', userId);
+    res.status(200).send('Cliente eliminado');
+  });
+};
 
 module.exports = {
   register,
-  login
-  //updateProfile
+  login,
+  updateProfile,
+  getUsers,
+  deleteUser
 };
