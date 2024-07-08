32a30339ef460377f8c716e900959b653c426e69
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-07-07 23:44:20 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware'); 
 
-// Rutas para registro, login y perfil
+// Ruta para el registro, login y perfil
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.put('/profile', authMiddleware, authController.updateProfile); // para actualizar perfil
