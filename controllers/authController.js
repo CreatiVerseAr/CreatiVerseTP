5698a46e2da0669271c32dfeb8ad6c781943fbb5
CynthiaVM <152813634+CreatiVerseAr@users.noreply.github.com>
2024-07-10 22:09:56 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,11 +10,11 @@ const register = async (req, res) => {
     const newUser = { nombre, apellido, genero, email, password: hashedPassword, nivel };
 
     User.create(newUser, (err, result) => {
-      if (err) return res.status(500).send('Error al registrar el cliente');
-      res.status(201).send('Cliente registrado');
+      if (err) return res.status(500).send('Error al registrar el usuario');
+      res.status(201).send('Usuario registrado');
     });
   } catch (error) {
-    res.status(500).send('Error al registrar el cliente');
+    res.status(500).send('Error al registrar el Usuario');
   }
 };
 
@@ -22,8 +22,8 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     User.findByEmail(email, async (err, user) => {
-      if (err) return res.status(500).send('Error al buscar el cliente');
-      if (!user) return res.status(404).send('Cliente no encontrado');
+      if (err) return res.status(500).send('Error al buscar el usuario');
+      if (!user) return res.status(404).send('Usuario no encontrado');
 
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) return res.status(401).send('Contraseña incorrecta');
@@ -43,18 +43,18 @@ const updateProfile = async (req, res) => {
     const updatedUser = { nombre, apellido, genero, email, password: hashedPassword, nivel };
 
     User.update(id, updatedUser, (err, result) => {
-      if (err) return res.status(500).send('Error al actualizar el cliente');
-      if (result.affectedRows === 0) return res.status(404).send('Cliente no encontrado');
-      res.status(200).send('Cliente actualizado');
+      if (err) return res.status(500).send('Error al actualizar el usuario');
+      if (result.affectedRows === 0) return res.status(404).send('Usuario no encontrado');
+      res.status(200).send('Usuario actualizado');
     });
   } catch (error) {
-    res.status(500).send('Error al actualizar el cliente');
+    res.status(500).send('Error al actualizar el usuario');
   }
 };
 // Para ver los usuarios
 const getUsers = (req, res) => {
   User.getAll((err, users) => {
-    if (err) return res.status(500).send('Error al obtener los clientes');
+    if (err) return res.status(500).send('Error al obtener los usuarios');
     res.status(200).json(users);
   });
 };
@@ -65,14 +65,14 @@ const deleteUser = (req, res) => {
   User.delete(userId, (err, result) => {
     if (err) {
       console.error('Error deleting user:', err);
-      return res.status(500).send('Error al eliminar el cliente');
+      return res.status(500).send('Error al eliminar el usuario');
     }
     if (result.affectedRows === 0) {
       console.log('User not found:', userId);
-      return res.status(404).send('Cliente no encontrado');
+      return res.status(404).send('Usuario no encontrado');
     }
     console.log('User deleted successfully:', userId);
-    res.status(200).send('Cliente eliminado');
+    res.status(200).send('Usuario eliminado');
   });
 };
 
